6e8f40a5a10539503df9e57ff678d7ec5ee98b72
jonassinjan <einsteinisbae@gmail.com>
2019-07-19 17:03:09 +0200

    added the correct version of plot_phi

--- Analysis Code/plot_phi.py
+++ Analysis Code/plot_phi.py
@@ -10,34 +10,45 @@ from numpy.random import rand
 from matplotlib import ticker
 from matplotlib import gridspec
 
-
-nx=129 #129 as 128 boxes but data comes with points for each edge
+nx=129
 ny=129
-dir_data = "../2d_displacement/data/" # into parent directory
+dir_data = "/lustre/fs23/group/that/jonas/DESY/2d_displacement/data/"
 
-filename=dir_data+'PHI'+'.BIN'
+filename=dir_data+'PHI0'+'.BIN'
 print(filename)  
 fd = open(filename, 'rb')
 
 abx = np.fromfile(file=fd,dtype=np.float64,count=nx*ny)
   
-temp = np.reshape(abx,(nx,ny))
+temp1 = np.reshape(abx,(nx,ny))
+
+filename=dir_data+'PHI'+'.BIN'
+print(filename)
+fd = open(filename, 'rb')
+
+abx = np.fromfile(file=fd,dtype=np.float64,count=nx*ny)
+
+temp2 = np.reshape(abx,(nx,ny))
+
 
 fig=plt.figure()
 fig = plt.figure(figsize=(5.0, 5.0))
-gs = gridspec.GridSpec(1, 1, hspace=0.2, wspace=0.2)
+gs = gridspec.GridSpec(2, 1, hspace=0.2, wspace=0.2)
 ax0 = plt.subplot(gs[0],aspect='equal')
-z=temp
+z=temp1
 plt.imshow(z, cmap='seismic', extent=[0, 1, 0, 1],
            interpolation='nearest', origin='lower')
-
 fig = plt.gcf()
 plt.clim()   # clamp the color limits
 plt.colorbar()
+
+ax1 = plt.subplot(gs[1],aspect='equal')
+z=temp2
+plt.imshow(z, cmap='seismic', extent=[0, 1, 0, 1],
+           interpolation='nearest', origin='lower')
+fig = plt.gcf()
+plt.clim()   # clamp the color limits
+plt.colorbar()
+
 plt.show()
 fig.savefig('test.eps',dpi=300,bbox_inches='tight')
-  
-  
-
-
-
