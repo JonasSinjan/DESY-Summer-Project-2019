e1a9bd295c66f5dd1baf63c8e8d5f4a2d6448b76
jonassinjan <einsteinisbae@gmail.com>
2019-07-23 16:45:36 +0200

    print plot_phi check

--- Code_Analysis/plot_phi.py
+++ Code_Analysis/plot_phi.py
@@ -22,7 +22,7 @@ abx = np.fromfile(file=fd,dtype=np.float64,count=nx*ny)
   
 temp1 = np.reshape(abx,(nx,ny))
 
-#print(temp1[:,22])
+print(temp1[:,22])
 
 filename=dir_data+'PHI'+'.BIN'
 print(filename)
