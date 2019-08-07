a57dd38bd48c0b83403526f54783e6e9477bee5b
jonassinjan <einsteinisbae@gmail.com>
2019-08-07 16:29:38 +0200

    check len arrays

--- Code_Analysis/plot_power.py
+++ Code_Analysis/plot_power.py
@@ -9,12 +9,14 @@ dir_output = "/lustre/fs23/group/that/jonas/Github_repo/DESY/2d_displacement/128
 # perpendicular
 filename = dir_data + 'PS_KT_PHI.DAT'
 ps_kperp = np.loadtxt(filename)
-log_ps_kperp = np.log(ps_kperp)
+log_ps_kperp = [np.log(i) for i in ps_kperp if i != 0]
 
 # parallel
 filename = dir_data + 'PS_KB_PHI.DAT'
 ps_kpar = np.loadtxt(filename)
-log_ps_kpar = np.log(ps_kpar)
+log_ps_kpar = [np.log(i) for i in ps_kpar if i != 0]
+
+print(f"len of ps_perp = {len(log_ps_kperp)}", f"len of ps_perp = {len(log_ps_kperp)}")
 
 start = -((n-1)/2 + 1)
 logk = [0]*len(range(n-2))
@@ -25,7 +27,10 @@ for i in range(n-2):
         continue
     logk[i] = np.log(tmp)
 
-plt.figure()
-plt.plot(logk, log_ps_kperp)
-plt.plot(logk, log_ps_kpar)
-plt.show()
+print(f"len of logk = {len(logk)}")
+
+#
+#plt.figure()
+#plt.plot(logk, log_ps_kperp)
+#plt.plot(logk, log_ps_kpar)
+#plt.show()
