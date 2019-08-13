87ce6feaf0cbb9c2fc6e003f297393853e872685
jonassinjan <33846422+JonasSinjan@users.noreply.github.com>
2019-08-13 13:33:26 +0200

    combined phi_fft.py and plot_power files + allowed kx in main.f08 of displacement method to be equal to zero

--- Code_Analysis/phi_fft.py
+++ Code_Analysis/phi_fft.py
@@ -19,8 +19,8 @@ from matplotlib import ticker
 from matplotlib import gridspec
 
 # data files
-# dir_data = "/lustre/fs23/group/that/jonas/Github_repo/DESY/2d_displacement/128run2D_73_test/"
-dir_data = "c:/Users/jonas/DESY/2d_displacement/Runs/128run2D_73/"
+dir_data = "/lustre/fs23/group/that/jonas/Github_repo/DESY/2d_displacement/128run2D_73_test/"
+#dir_data = "c:/Users/jonas/DESY/2d_displacement/Runs/128run2D_73_test/"
 
 # 2D
 n = 128
@@ -67,34 +67,34 @@ for i in range(20):
 # print(tmp_arr)
 
 
-# fig = plt.figure()
-# fig = plt.figure(figsize=(5.0, 5.0))
-# gs = gridspec.GridSpec(2, 1, hspace=0.5, wspace=0.2)
-#
-# ax0 = plt.subplot(gs[0], aspect='equal')
-#
-# plt.imshow(np.log(abs(phi0k) ** 2), cmap='seismic',
-#            interpolation='nearest', origin='lower')
-# # plt.imshow(tmp_arr)
-# fig = plt.gcf()  # get the current figure
-# plt.clim()  # clamp the color limits
-# plt.colorbar()
-# plt.ylabel('K_parallel')
-# plt.xlabel('K_perp')
-# plt.title('FFT of Phi0')
-#
-# ax1 = plt.subplot(gs[1], aspect='equal')
-#
-# plt.imshow(np.log(abs(phik) ** 2), cmap='seismic',
-#            interpolation='nearest', origin='lower')
-# fig = plt.gcf()  # get current figure
-# plt.clim()  # clamp the color limits
-# plt.colorbar()
-# plt.ylabel('K_parallel')
-# plt.xlabel('K_perp')
-# plt.title('FFT of Phi')
-# plt.savefig('test')
-# plt.show()
+fig = plt.figure(1)
+fig = plt.figure(figsize=(5.0, 5.0))
+gs = gridspec.GridSpec(2, 1, hspace=0.5, wspace=0.2)
+
+ax0 = plt.subplot(gs[0], aspect='equal')
+
+plt.imshow(np.log(abs(phi0k) ** 2), cmap='seismic',
+           interpolation='nearest', origin='lower')
+# plt.imshow(tmp_arr)
+fig = plt.gcf()  # get the current figure
+plt.clim()  # clamp the color limits
+plt.colorbar()
+plt.ylabel('K_parallel')
+plt.xlabel('K_perp')
+plt.title('FFT of Phi0')
+
+ax1 = plt.subplot(gs[1], aspect='equal')
+
+plt.imshow(np.log(abs(phik) ** 2), cmap='seismic',
+           interpolation='nearest', origin='lower')
+fig = plt.gcf()  # get current figure
+plt.clim()  # clamp the color limits
+plt.colorbar()
+plt.ylabel('K_parallel')
+plt.xlabel('K_perp')
+plt.title('FFT of Phi')
+plt.savefig('test')
+plt.show()
 
 perp_spectrum = np.zeros(nx)
 para_spectrum = np.zeros(nx)
@@ -102,30 +102,75 @@ for i in range(nx):
     perp_spectrum[i] = np.sum(abs(phik[:, i]) ** 2)
     para_spectrum[i] = np.sum(abs(phik[i, :]) ** 2)
 
-para_first = para_spectrum[:64]
-para_second = para_spectrum[65:]
+para_first = para_spectrum[:int(n/2)]
+para_second = para_spectrum[int(n/2 +1) :]
 para_flipped = np.flip(para_first)
 
-perp_first = perp_spectrum[:64]  # skip nyquist
-perp_second = perp_spectrum[65:]
+perp_first = perp_spectrum[:int(n/2)]  # skip nyquist
+perp_second = perp_spectrum[int(n/2 +1):]
 perp_flipped = np.flip(perp_first)
 
-perp_total = (perp_second + perp_flipped) /2.0
-para_total = (para_second + para_flipped) /2.0
+perp_total = (perp_second + perp_flipped) / 2.0
+para_total = (para_second + para_flipped) / 2.0
 
 start = 5
 end = 40
 
 slope, intercept, rval, p, err = linregress(np.log(range(start, end)), np.log(perp_total[start:end]))
-slope_par, intercept_par, rval_pa, p_para, err_para = linregress(np.log(range(start, end)), np.log(para_total[start:end]))
+slope_par, intercept_par, rval_pa, p_para, err_para = linregress(np.log(range(start, end)),
+                                                                 np.log(para_total[start:end]))
 
-lin_perb = [slope*i + intercept for i in np.log(range(1, 64))]
-lin_par = [slope_par*i + intercept_par for i in np.log(range(1, 64))]
+lin_perb = [slope * i + intercept for i in np.log(range(1, int(n/2)))]
+lin_par = [slope_par * i + intercept_par for i in np.log(range(1, int(n/2)))]
 
 plt.figure(2)
 plt.plot(np.log(range(start, end)), np.log(perp_total[start:end]), label='Perp')
 plt.plot(np.log(range(start, end)), np.log(para_total[start:end]), label='Para')
 plt.legend()
-plt.show()
+#plt.show()
 
 print(slope, slope_par)
+
+# Plotting PS_KT files from power_kk method in spectrum.f08
+
+# n = 128
+#
+# #dir_data = "/lustre/fs23/group/that/jonas/Github_repo/DESY/2d_displacement/Runs/128run2D_73/power_spectra/"  # data files
+# dir_data = "c:/Users/jonas/DESY/2d_displacement/Runs/128run2D_73/power_spectra/"
+#
+# # perpendicular
+# filename = dir_data + 'PS_KT_PHI.DAT'
+# ps_kperp = np.loadtxt(filename)
+# log_ps_kperp = [np.log(i) for i in ps_kperp[:, 1] if i.any() != 0]
+#
+# # parallel
+# filename = dir_data + 'PS_KB_PHI.DAT'
+# ps_kpar = np.loadtxt(filename)
+# log_ps_kpar = [np.log(i) for i in ps_kpar[:, 1] if i.any() != 0]
+#
+# #print(log_ps_kpar[1:])
+# #print(log_ps_kperp[1:])
+#
+# logk = np.log(range(int((n-1) / 2 )))
+#
+# #print(logk)
+#
+# slope, intercept, rval, p, err = linregress(logk[start:end], log_ps_kperp[start:end])
+# slope_par, intercept_par, rval_pa, p_para, err_para = linregress(logk[start:end], log_ps_kpar[start:end])
+#
+# lin_perb = [slope * i + intercept for i in logk[start:end]]
+# lin_par = [slope_par * i + intercept_par for i in logk[start:end]]
+#
+# print(slope, slope_par)
+#
+# plt.figure(2)
+# plt.scatter(logk[start:end], log_ps_kperp[start:end], label='Kperp', color='blue')
+# plt.scatter(logk[start:end], log_ps_kpar[start:end], label='Kpara', color='red')
+# plt.plot(logk[start:end], lin_perb, label='Slope K_perp: %f' % round(slope, 3))
+# plt.plot(logk[start:end], lin_par, label='Slope K_para: %f' % round(slope_par, 3))
+#
+# plt.xlabel('Log K')
+# plt.ylabel('Log E(k)')
+#
+# plt.legend()
+# plt.show()
