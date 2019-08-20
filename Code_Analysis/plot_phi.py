92e0b5306e45c51a132385ef7ebd3ef6c4ea668a
jonassinjan <einsteinisbae@gmail.com>
2019-08-20 15:01:03 +0200

    3d plot attempt

--- Code_Analysis/plot_phi.py
+++ Code_Analysis/plot_phi.py
@@ -9,6 +9,7 @@ from numpy.random import randint
 from numpy.random import rand
 from matplotlib import ticker
 from matplotlib import gridspec
+from mpl_toolkits.mplot3d import Axes3D
 
 res = 64
 nx=res
@@ -37,22 +38,33 @@ temp2 = np.reshape(abx,(nx,ny,nz))
 fig=plt.figure()
 fig = plt.figure(figsize=(5.0, 5.0))
 gs = gridspec.GridSpec(2, 1, hspace=0.2, wspace=0.2)
-ax0 = plt.subplot(gs[0],aspect='equal')
+ax0 = plt.subplot(gs[0],aspect='equal',projection='3d')
 
-z=temp1[2,:,:] #one slice
+z=temp1#[2,:,:] #one slice
 
-plt.imshow(z, cmap='seismic', extent=[0, 1, 0, 1],
-           interpolation='nearest', origin='lower')
+#plt.imshow(z, cmap='seismic', extent=[0, 1, 0, 1],
+#           interpolation='nearest', origin='lower')
+x = np.arange(z.shape[0])[:, None, None]
+y = np.arange(z.shape[1])[None, :, None]
+z = np.arange(z.shape[2])[None, None, :]
+x, y, z = np.broadcast_arrays(x, y, z)
+c = np.tile(z.ravel()[:, None], [1, 3])
+
+ax0.scatter(x.ravel(),
+           y.ravel(),
+           z.ravel(),c=c)
+
 fig = plt.gcf()
 plt.clim()   # clamp the color limits
 plt.colorbar()
 
 ax1 = plt.subplot(gs[1],aspect='equal')
 
-z=temp2[2,:,:] #one slice
+z=temp2#[2,:,:] #one slice
 
-plt.imshow(z, cmap='seismic', extent=[0, 1, 0, 1],
-           interpolation='nearest', origin='lower')
+#plt.imshow(z, cmap='seismic', extent=[0, 1, 0, 1],
+#           interpolation='nearest', origin='lower')
+
 fig = plt.gcf()
 plt.clim()   # clamp the color limits
 plt.colorbar()
