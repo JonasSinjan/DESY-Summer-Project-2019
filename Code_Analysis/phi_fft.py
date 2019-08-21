abacd8c0daea56118e5c29f1d7923ad43e3dce82
Jonas Sinjan <sinjan@wgs20.zeuthen.desy.de>
2019-08-21 15:43:45 +0200

    testing both 2d and 3d

--- Code_Analysis/phi_fft.py
+++ Code_Analysis/phi_fft.py
@@ -253,7 +253,8 @@ def plot_power3d(dir1, n):
 
     for i in range(nx):
         para_spectrum[i] = np.sum(abs(phi0k[i, :, :]) ** 2)
-    
+	
+    print(para_spectrum)    
     para_first = para_spectrum[:int(n / 2)]
     para_second = para_spectrum[int(n / 2 + 1):]
     para_flipped = np.flip(para_first)
@@ -282,16 +283,18 @@ def plot_power3d(dir1, n):
 
 if __name__ == "__main__":
 
-    n = 64
+    n = 128
     # dir_data1 = "c:/Users/jonas/DESY/2d_displacement/Runs/128run2D_73_test/"  # this one allows kx == 0
     # dir_data2 = "c:/Users/jonas/DESY/2d_displacement/Runs/128run2D_73_test/power_spectra/"
     # plot_power(dir_data1, dir_data2, n)
 
-    # dir_data3 = "c:/Users/jonas/DESY/2d_displacement/Runs/128run2D_73_frac/"
-    # dir_data4 = "c:/Users/jonas/DESY/2d_displacement/Runs/128run2D_73_frac/power_spectra/"
-    # plot_power2d(dir_data3, dir_data4, n)
+    dir_data3 = "/lustre/fs23/group/that/jonas/Github_repo/DESY/2d_displacement/Runs/128run2D_73_frac/"
+    dir_data4 = "/lustre/fs23/group/that/jonas/Github_repo/DESY/2d_displacement/Runs/128run2D_73_frac/power_spectra/"
+    plot_power2d(dir_data3, dir_data4, n)
+
+    n = 64
     dir_data = "/lustre/fs23/group/that/jonas/Github_repo/DESY/3d_displacement/64run3D/"
     plot_power3d(dir_data,n)
 
 # #dir_data = "/lustre/fs23/group/that/jonas/Github_repo/DESY/2d_displacement/Runs/128run2D_73/power_spectra/"
-# dir_data = "c:/Users/jonas/DESY/2d_displacement/Runs/128run2D_73_test/power_spectra/"
+# dir__data = "c:/Users/jonas/DESY/2d_displacement/Runs/128run2D_73_test/power_spectra/"
