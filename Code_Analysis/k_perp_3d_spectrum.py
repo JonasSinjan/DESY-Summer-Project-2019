9d207ba6898d595c5b8ba415e00c49a1c4486c8a
jonassinjan <einsteinisbae@gmail.com>
2019-08-21 15:59:31 +0200

    renaming variables and titling plots

--- Code_Analysis/k_perp_3d_spectrum.py
+++ Code_Analysis/k_perp_3d_spectrum.py
@@ -18,8 +18,8 @@ def kspec_funk(ff):
     pwr_f = 0.0
     
     for theta in theta_array :
-        kxx = phikperp[ff]*np.cos(theta) #kxx in that plane - actually ky and kz(perp)
-        kyy = phikperp[ff]*np.sin(theta) 
+        kxx = kperp_rad[ff]*np.cos(theta) #kxx in that plane - actually ky and kz(perp)
+        kyy = kperp_rad[ff]*np.sin(theta) 
         pont=(kxx,kyy)
         pwr_f = pwr_f + np.exp(logp_interp(pont))
         count=count+1
@@ -27,14 +27,16 @@ def kspec_funk(ff):
     #taking shell average
     pwr_f = pwr_f/count
     
-    #multiplying by shell area to get 1D power 
-    f_pow = pwr_f*2.0*np.pi*phikperp[ff]
+    #multiplying by shell area to get 1D power
+    # THIS ISNT SHELL AREA? 
+    f_pow = pwr_f*2.0*np.pi*kperp_rad[ff]
 
     return [f_pow]
 
 def slice_fft(sn,phi):
     fx2d = phi[:,:,sn]
-    
+    # WHY ARE THEY SLICING? AND WHY IN LAST INDEX- WHICH INDEX SHOULD IT BE?
+
     #calculating the FFT
     fxk = np.fft.rfftn(fx2d)
    
@@ -74,17 +76,16 @@ def k_perp_calculator(n,phi,phi0, dir_data):
 
     RGI = spint.RegularGridInterpolator
 
-    phikperp = np.zeros(n/2)
+    kperp_rad = np.zeros(n/2)
     phipower_spec = np.zeros(n/2)
     f_power_spec = np.zeros(n/2)
     pfolded = np.zeros((xpt/2,ypt/2))
     logpfolded = np.zeros((xpt/2,ypt/2))
 
-    pool = Pool(processes=nprocsfft) 
-    oter = pool.map(slice_fft, np.arange(0,n,n/nzslices))
+    for i in np.arange(0,n,n/nzslices):
+        oter = slice_fft(i, phi0)
+    
     fftslices = np.asarray(oter)
-    pool.terminate()
-
     pk_slice = np.mean(fftslices,axis=0)
 
     #folding along the y axis
@@ -96,21 +97,20 @@ def k_perp_calculator(n,phi,phi0, dir_data):
     logpfolded = np.log(pfolded)
 
     #defining the axes of the 3d spec array
-    kxarr = np.linspace(0.0,(xpt/2-1)*2.0*np.pi,num=xpt/2)
-    kyarr = np.linspace(0.0,(ypt/2-1)*2.0*np.pi,num=ypt/2)
+    kyarr = np.linspace(0.0,(xpt/2-1),num=xpt/2)
+    kzarr = np.linspace(0.0,(ypt/2-1),num=ypt/2)
 
     #making the interpolation function
-    logp_interp = RGI(points=[kxarr,kyarr], values=logpfolded)
+    logp_interp = RGI(points=[kyarr,kzarr], values=logpfolded)
 
     #making the spectrum now
     #zero wavenumber is special case
-    phikperp[0] = 0.0
+    kperp_rad[0] = 0.0
     pont = (0.0,0.0)
     f_power_spec[0] = np.exp(logp_interp(pont))
 
-    #looping over the higher wavenumbers
-    for i in range (1,n/2) :
-        phikperp[i] = i*2.0*np.pi
+    #looping over the higher wavenumbers? - this is the radius
+    kperp_rad = range(1,n/2)
 
     pool = Pool(processes=nprocs) 
     kspecs = pool.map(kspec_funk, np.arange(1,n/2))
@@ -122,7 +122,7 @@ def k_perp_calculator(n,phi,phi0, dir_data):
     #writing the spectra to a file
     f=open(dir_data+mode+'_kperp_spec.txt','w')
     for i in range(0,n/2):
-        value=str(phikperp[i]) +" "+str(f_power_spec[i])
+        value=str(kperp_rad[i]) +" "+str(f_power_spec[i])
         f.write(value+"\n")
         f.close()
 
