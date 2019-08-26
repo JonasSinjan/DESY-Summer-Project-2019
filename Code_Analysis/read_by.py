f14ec59bc636c72ddf76d8d1f63d2f55a14651d9
jonassinjan <einsteinisbae@gmail.com>
2019-08-26 16:29:45 +0200

    add test file

---
+++ Code_Analysis/read_by.py
@@ -0,0 +1,26 @@
+import struct
+import numpy as np
+import matplotlib.pyplot as plt
+from multiprocessing import Pool
+import math
+import scipy.interpolate as spint
+from numpy.random import seed
+from numpy.random import randint
+from numpy.random import rand
+from matplotlib import ticker
+from matplotlib import gridspec
+from mpl_toolkits.mplot3d import Axes3D
+
+
+dir_data = "/lustre/fs23/group/that/jonas/Github_repo/DESY/3d_displacement/128_test/"
+n=128
+nx = n
+ny = n
+nz = n
+filename=dir_data+'BX'+'.BIN'
+print(filename)
+fd = open(filename, 'rb')
+abx = np.fromfile(file=fd,dtype=np.float64,count=nx*ny*nz)
+temp3 = np.reshape(abx,(nx,ny,nz))
+
+print(temp3[12,:,34])
