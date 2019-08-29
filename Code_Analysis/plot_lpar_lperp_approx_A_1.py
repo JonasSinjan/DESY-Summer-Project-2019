c23a0113bf98b099681ad2e0295f18b3491ae4c8
Jonas Sinjan <einsteinisbae@gmail.com>
2019-08-29 11:23:09 +0100

    fig

--- Code_Analysis/plot_lpar_lperp_approx_A_1.py
+++ Code_Analysis/plot_lpar_lperp_approx_A_1.py
@@ -357,7 +357,7 @@ slope_512_sq, rval_512_sq, err_512_sq = linfit(lperp8,lpar8, count_512sq)
 #--------------------------------------------------------------------------------------------------------------------------------------------
 
 
-fig=plt.figure(1)
+fig=plt.figure()
 fig = plt.figure(figsize=(16.0, 10.0))
 gs = gridspec.GridSpec(1, 1, hspace=0.0, wspace=0.0)
 
