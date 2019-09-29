56878ea089ff8b1e120c0b7709047eddeff6d60c
Jonas Sinjan <einsteinisbae@gmail.com>
2019-09-29 22:49:44 +0100

    first attempt 2dstrucfunk alt

--- Code_Analysis/2D alt struc funk/calc_sf2D_modes_parallel.py
+++ Code_Analysis/2D alt struc funk/calc_sf2D_modes_parallel.py
@@ -9,31 +9,60 @@ from numpy.random import randint
 from numpy.random import rand
 #from pylab import *
 
-lent=1024
-xpt=lent
-ypt=lent
-zpt=lent
+lent=128
+nx=lent
+ny=lent
+nz=lent
 Lx=1.0
 Ly=1.0
 Lz=1.0
-t_start = 2
-t_stop = 6
-step = 1
-dir_data = "../1024_runs/C4/data/decomped_modes/"
-dir_output = "../1024_runs/C4/data/decomped_modes/"
+
+working_dir_path = '/home/jonas/Documents/VSCode/DESY/'
+
+dir_data = working_dir_path + 'final_data/3d/128run3D_FFT/'
+dir_output = working_dir_path + "final_data/3d/128run3D_FFT/"
+
 seed(1)
 n_avg_bfield_pts = 5
 nrandpts = 2000
-nprocs = 24
+
 mode= 'F'
 
 ntstp = 0
 sf2D_array=np.zeros((lent/4,lent/4))
 
-def struct2D_funk(ipar):
+def read_b_files3D(dir_data):
+
+  filename = dir_data + 'BX' + '.BIN' 
+  fd = open(filename, 'rb')
+
+  abx = np.fromfile(file=fd, dtype=np.float64, count=nx * ny * nz)
+
+  temp = np.reshape(abx, (nx, ny, nz))
+  bx = temp.transpose()
+
+  filename = dir_data + 'BY' + '.BIN'
+  fd = open(filename, 'rb')
+
+  aby = np.fromfile(file=fd, dtype=np.float64, count=nx * ny * nz)
+
+  temp = np.reshape(aby, (nx, ny, nz))
+  by = temp.transpose()
+
+  filename = dir_data + 'BZ' + '.BIN'
+  fd = open(filename, 'rb')
+
+  aby = np.fromfile(file=fd, dtype=np.float64, count=nx * ny * nz)
+
+  temp = np.reshape(aby, (nx, ny, nz))
+  bz = temp.transpose()
+
+  return bx, by, bz
+
+def struct2D_funk(ipar, bx, by, bz):
 
   lpar = ipar*1.0
-  print("ipar = ", ipar)
+  print('ipar = ', ipar)
 
   sf_lperp = np.zeros(lent/4)
 
@@ -54,6 +83,7 @@ def struct2D_funk(ipar):
 
       #calculate the average b field direction in a sphere of radius sp_rad around random point (xi,yi,zi)
       #do this by looping over npts_avg_field
+      #print(rand(n_avg_bfield_pts))
       lr = rand(n_avg_bfield_pts)*sp_rad
       theta = rand(n_avg_bfield_pts)*np.pi
       phi   = rand(n_avg_bfield_pts)*2.0*np.pi
@@ -96,7 +126,7 @@ def struct2D_funk(ipar):
       diff = b1-b2
       summb = np.sum(diff*diff)
       if (math.isinf(summb) or math.isnan(summb)):
-        print("isinf isnan")
+        print('isinf isnan')
         continue
   
       ssff = ssff + summb
@@ -106,96 +136,24 @@ def struct2D_funk(ipar):
 
   return sf_lperp
 
- 
-  """
-
-    #bhat contains the unit vector along the local magnetic field
-    #now take 2 points separated by distance ll along this direction with center at ri
-    r1 = np.int_(ri+(ll/2.0)*bhat)
-    r2 = np.int_(ri-(ll/2.0)*bhat)
-
-    #renormalize indices
-    r1 = r1%lent
-    r2 = r2%lent
-
-    b1 = np.array([  bx[r1[0],r1[1],r1[2]]  , by[r1[0],r1[1],r1[2]] , bz[r1[0],r1[1],r1[2]]   ])
-    b2 = np.array([  bx[r2[0],r2[1],r2[2]]  , by[r2[0],r2[1],r2[2]] , bz[r2[0],r2[1],r2[2]]   ])
-
-    sf_pare = sf_pare + np.sum((b1-b2)*(b1-b2))
-
-    #next calculating the perpendicular structure function
-    #make a unit vector perpendicular to bhat
-    #generate another random vector
-    vrand = rand(3)
-    #taking cross product with bhat to generate perpendicular vector
-    perpb_x = bhat[1]*vrand[2]-bhat[2]*vrand[1]
-    perpb_y = bhat[2]*vrand[0]-bhat[0]*vrand[2]
-    perpb_z = bhat[0]*vrand[1]-bhat[1]*vrand[0]
-    perpb = np.array([ perpb_x, perpb_y, perpb_z ])
-    perpb = perpb/(np.sqrt(np.sum(perpb*perpb)))
-
-    #now take 2 points separated by distance ll along the perpendicular direction with center at ri
-    r1 = np.int_(ri+(ll/2.0)*perpb)
-    r2 = np.int_(ri-(ll/2.0)*perpb)
-
-    #renormalize indices
-    r1 = r1%lent
-    r2 = r2%lent
-
-    b1 = np.array([  bx[r1[0],r1[1],r1[2]]  , by[r1[0],r1[1],r1[2]] , bz[r1[0],r1[1],r1[2]]   ])
-    b2 = np.array([  bx[r2[0],r2[1],r2[2]]  , by[r2[0],r2[1],r2[2]] , bz[r2[0],r2[1],r2[2]]   ])
-
-    sf_perpe = sf_perpe + np.sum((b1-b2)*(b1-b2))
-
-    numpt = numpt + 1.0
-
-  #print(ll,numpt,sf_pare,sf_perpe)
-  return [numpt,sf_pare,sf_perpe]
-
-  """
-
-for t in range (t_start,t_stop+1,step) : #the time loop 
-  
-  filename=dir_data+'B'+mode+str(t)+'.BIN'
-  print(filename)
-  fd = open(filename, 'rb')
-  fd.read(4)
-  nx = np.fromfile(file=fd,dtype=np.int32,count=1)[0]
-  ny = np.fromfile(file=fd,dtype=np.int32,count=1)[0]
-  nz = np.fromfile(file=fd,dtype=np.int32,count=1)[0]
-  print(nx,ny,nz)
-  fd.read(4)
-  fd.read(4)
-  abx = np.fromfile(file=fd,dtype=np.float64,count=nx*ny*nz)
-  fd.read(4)
-  fd.read(4)
-  aby = np.fromfile(file=fd,dtype=np.float64,count=nx*ny*nz)
-  fd.read(4)
-  fd.read(4)
-  abz = np.fromfile(file=fd,dtype=np.float64,count=nx*ny*nz)
-  fd.read(4)
-
-  temp = np.reshape(abx,(lent,lent,lent))
-  bx = temp.transpose()
-  temp = np.reshape(aby,(lent,lent,lent))
-  by = temp.transpose()
-  temp = np.reshape(abz,(lent,lent,lent))
-  bz = temp.transpose()
-
   #print(np.amax(bx),np.amax(by),np.amax(bz))
 
-  if __name__ == '__main__':  
-    pool = Pool(processes=nprocs) 
-    sf2D_list = pool.map(struct2D_funk, range(lent/4))
-    sf2D_arr = np.asarray(sf2D_list)
-    pool.terminate() 
+if __name__ == '__main__': 
+  sf2D_list=[0]*int(lent / 4)
+  bx,by,bz = read_b_files3D(dir_data) 
 
-  sf2D_array = sf2D_array + sf2D_arr
-  ntstp = ntstp + 1
+  for i in range(int(lent / 4)):
+    sf2D_list[i] = struct2D_funk(i, bx,by,bz)
+  
+  sf2D_arr = np.asarray(sf2D_list)
+  
+
+sf2D_array = sf2D_array + sf2D_arr
+ntstp = ntstp + 1
 
 sf2D_array = sf2D_array/ntstp
 
-np.save(dir_output+'sf2D_'+mode+'.npy',sf2D_array)
+np.save(dir_output+'sf2D_.npy',sf2D_array)
 
 """
 sf_par = sf_par/npts
--- Code_Analysis/2D alt struc funk/plot_sf2D_A.py
+++ Code_Analysis/2D alt struc funk/plot_sf2D_A.py
@@ -26,59 +26,61 @@ def smoothing(xarr) :
 
   return xarr
 
+working_dir_path = '/home/jonas/Documents/VSCode/DESY/'
 
-sf2D_array = np.load('../1024_runs/S1/data/decomped_modes/sf2D_A.npy')
-lent=1024
+sf2D_array = np.load(working_dir_path + 'final_data/3d/128run3D_FFT/sf2D_.npy')
+lent=128
 lpar1 = 1.0*np.arange(lent/4)/lent
 lperp1 = 1.0*np.arange(lent/4)/lent
 sf2D1 = smoothing(sf2D_array)
 
-sf2D_array = np.load('../1024_runs/S4/data/decomped_modes/sf2D_A.npy')
-lent=1024
-lpar2 = 1.0*np.arange(lent/4)/lent
-lperp2 = 1.0*np.arange(lent/4)/lent
-sf2D2 = smoothing(sf2D_array)
+# sf2D_array = np.load('../1024_runs/S4/data/decomped_modes/sf2D_A.npy')
+# lent=1024
+# lpar2 = 1.0*np.arange(lent/4)/lent
+# lperp2 = 1.0*np.arange(lent/4)/lent
+# sf2D2 = smoothing(sf2D_array)
 
-sf2D_array = np.load('../512_runs_4/C2/data/decomped_modes/sf2D_A.npy')
-lent=512
-lpar3 = 1.0*np.arange(lent/4)/lent
-lperp3 = 1.0*np.arange(lent/4)/lent
-sf2D3 = smoothing(sf2D_array)
+# sf2D_array = np.load('../512_runs_4/C2/data/decomped_modes/sf2D_A.npy')
+# lent=512
+# lpar3 = 1.0*np.arange(lent/4)/lent
+# lperp3 = 1.0*np.arange(lent/4)/lent
+# sf2D3 = smoothing(sf2D_array)
 
-sf2D_array = np.load('../512_runs_4/CB1/data/decomped_modes/sf2D_A.npy')
-lent=512
-lpar4 = 1.0*np.arange(lent/4)/lent
-lperp4 = 1.0*np.arange(lent/4)/lent
-sf2D4 = smoothing(sf2D_array)
+# sf2D_array = np.load('../512_runs_4/CB1/data/decomped_modes/sf2D_A.npy')
+# lent=512
+# lpar4 = 1.0*np.arange(lent/4)/lent
+# lperp4 = 1.0*np.arange(lent/4)/lent
+# sf2D4 = smoothing(sf2D_array)
 
-sf2D_array = np.load('../1024_runs/S1/data/decomped_modes/sf2D_v_A.npy')
-lent=1024
-lpar5 = 1.0*np.arange(lent/4)/lent
-lperp5 = 1.0*np.arange(lent/4)/lent
-sf2D5 = smoothing(sf2D_array)
+# sf2D_array = np.load('../1024_runs/S1/data/decomped_modes/sf2D_v_A.npy')
+# lent=1024
+# lpar5 = 1.0*np.arange(lent/4)/lent
+# lperp5 = 1.0*np.arange(lent/4)/lent
+# sf2D5 = smoothing(sf2D_array)
 
-sf2D_array = np.load('../1024_runs/S4/data/decomped_modes/sf2D_v_A.npy')
-lent=1024
-lpar6 = 1.0*np.arange(lent/4)/lent
-lperp6 = 1.0*np.arange(lent/4)/lent
-sf2D6 = smoothing(sf2D_array)
+# sf2D_array = np.load('../1024_runs/S4/data/decomped_modes/sf2D_v_A.npy')
+# lent=1024
+# lpar6 = 1.0*np.arange(lent/4)/lent
+# lperp6 = 1.0*np.arange(lent/4)/lent
+# sf2D6 = smoothing(sf2D_array)
 
-sf2D_array = np.load('../512_runs_4/C2/data/decomped_modes/sf2D_v_A.npy')
-lent=512
-lpar7 = 1.0*np.arange(lent/4)/lent
-lperp7 = 1.0*np.arange(lent/4)/lent
-sf2D7 = smoothing(sf2D_array)
+# sf2D_array = np.load('../512_runs_4/C2/data/decomped_modes/sf2D_v_A.npy')
+# lent=512
+# lpar7 = 1.0*np.arange(lent/4)/lent
+# lperp7 = 1.0*np.arange(lent/4)/lent
+# sf2D7 = smoothing(sf2D_array)
 
-sf2D_array = np.load('../512_runs_4/CB1/data/decomped_modes/sf2D_v_A.npy')
-lent=512
-lpar8 = 1.0*np.arange(lent/4)/lent
-lperp8 = 1.0*np.arange(lent/4)/lent
-sf2D8 = smoothing(sf2D_array)
+# sf2D_array = np.load('../512_runs_4/CB1/data/decomped_modes/sf2D_v_A.npy')
+# lent=512
+# lpar8 = 1.0*np.arange(lent/4)/lent
+# lperp8 = 1.0*np.arange(lent/4)/lent
+# sf2D8 = smoothing(sf2D_array)
 
 
 fig=plt.figure()
 fig = plt.figure(figsize=(24.0, 12.0))
-gs = gridspec.GridSpec(2, 4, hspace=0.0, wspace=0.1)
+# gs = gridspec.GridSpec(2, 4, hspace=0.0, wspace=0.1)
+gs = gridspec.GridSpec(1, 1, hspace=0.0, wspace=0.1)
 
 ax0 = plt.subplot(gs[0],aspect='equal')
 ax0.set_xlim(xmax=0.2)
@@ -87,7 +89,7 @@ ax0.set_ylim(ymax=0.2)
 sff2d = sf2D1
 lpar=lpar1
 lperp=lperp1
-cs=ax0.contour(lpar,lperp,np.transpose(sff2d),levels=[sff2d[5,5],sff2d[5,10],sff2d[5,20],sff2d[5,40],sff2d[5,60],sff2d[5,80],sff2d[5,100],sff2d[5,150],sff2d[5,200]],linewidths=2)
+cs=ax0.contour(lpar,lperp,np.transpose(sff2d),levels=[sff2d[5,5],sff2d[5,10],sff2d[5,20]],linewidths=2)
 #plt.clabel(cs,inline=1,fontsize=10)
 ax0.axes.xaxis.set_ticklabels([])
 ax0.set_title('(a) S1b B field',fontsize=18)
@@ -96,125 +98,125 @@ ax0.set_ylabel(r'$l_{\perp}/L$',fontsize=18)
 #ax0.legend(loc='bottom left')
 
 
-ax1 = plt.subplot(gs[1],aspect='equal')
-ax1.set_xlim(xmax=0.2)
-#ax0.set_ylim(ymin=10.0)
-ax1.set_ylim(ymax=0.2)
-sff2d = sf2D2
-lpar=lpar2
-lperp=lperp2
-cs=ax1.contour(lpar,lperp,np.transpose(sff2d),levels=[sff2d[5,5],sff2d[5,10],sff2d[5,20],sff2d[5,40],sff2d[5,60],sff2d[5,80],sff2d[5,100],sff2d[5,150],sff2d[5,200]],linewidths=2)
-#plt.clabel(cs,inline=1,fontsize=10)
-ax1.axes.xaxis.set_ticklabels([])
-ax1.axes.yaxis.set_ticklabels([])
-ax1.set_title('(b) S4b B field',fontsize=18)
-#ax1.set_ylabel(r'$l_{\perp}$',fontsize=16)
-#ax1.set_xlabel('$l_{\parallel}$',fontsize=16)
-#ax0.legend(loc='bottom left')
+# ax1 = plt.subplot(gs[1],aspect='equal')
+# ax1.set_xlim(xmax=0.2)
+# #ax0.set_ylim(ymin=10.0)
+# ax1.set_ylim(ymax=0.2)
+# sff2d = sf2D2
+# lpar=lpar2
+# lperp=lperp2
+# cs=ax1.contour(lpar,lperp,np.transpose(sff2d),levels=[sff2d[5,5],sff2d[5,10],sff2d[5,20],sff2d[5,40],sff2d[5,60],sff2d[5,80],sff2d[5,100],sff2d[5,150],sff2d[5,200]],linewidths=2)
+# #plt.clabel(cs,inline=1,fontsize=10)
+# ax1.axes.xaxis.set_ticklabels([])
+# ax1.axes.yaxis.set_ticklabels([])
+# ax1.set_title('(b) S4b B field',fontsize=18)
+# #ax1.set_ylabel(r'$l_{\perp}$',fontsize=16)
+# #ax1.set_xlabel('$l_{\parallel}$',fontsize=16)
+# #ax0.legend(loc='bottom left')
 
 
-ax2 = plt.subplot(gs[2],aspect='equal')
-ax2.set_xlim(xmax=0.2)
-#ax0.set_ylim(ymin=10.0)
-ax2.set_ylim(ymax=0.2)
-sff2d = sf2D3
-lpar=lpar3
-lperp=lperp3
-cs=ax2.contour(lpar,lperp,np.transpose(sff2d),levels=[sff2d[5,5],sff2d[5,10],sff2d[5,20],sff2d[5,40],sff2d[5,60],sff2d[5,80],sff2d[5,100]],linewidths=2)
-#plt.clabel(cs,inline=1,fontsize=10)
-ax2.axes.xaxis.set_ticklabels([])
-ax2.axes.yaxis.set_ticklabels([])
-ax2.set_title('(c) C2a B field',fontsize=18)
-ax2.axes.xaxis.set_ticklabels([])
-ax2.axes.yaxis.set_ticklabels([])
-#ax2.set_ylabel(r'$l_{\perp}$',fontsize=16)
-#ax2.set_xlabel('$l_{\parallel}$',fontsize=16)
-#ax0.legend(loc='bottom left')
-#plt.axvline(x=25.2)
+# ax2 = plt.subplot(gs[2],aspect='equal')
+# ax2.set_xlim(xmax=0.2)
+# #ax0.set_ylim(ymin=10.0)
+# ax2.set_ylim(ymax=0.2)
+# sff2d = sf2D3
+# lpar=lpar3
+# lperp=lperp3
+# cs=ax2.contour(lpar,lperp,np.transpose(sff2d),levels=[sff2d[5,5],sff2d[5,10],sff2d[5,20],sff2d[5,40],sff2d[5,60],sff2d[5,80],sff2d[5,100]],linewidths=2)
+# #plt.clabel(cs,inline=1,fontsize=10)
+# ax2.axes.xaxis.set_ticklabels([])
+# ax2.axes.yaxis.set_ticklabels([])
+# ax2.set_title('(c) C2a B field',fontsize=18)
+# ax2.axes.xaxis.set_ticklabels([])
+# ax2.axes.yaxis.set_ticklabels([])
+# #ax2.set_ylabel(r'$l_{\perp}$',fontsize=16)
+# #ax2.set_xlabel('$l_{\parallel}$',fontsize=16)
+# #ax0.legend(loc='bottom left')
+# #plt.axvline(x=25.2)
 
-ax3 = plt.subplot(gs[3],aspect='equal')
-ax3.set_xlim(xmax=0.2)
-#ax0.set_ylim(ymin=10.0)
-ax3.set_ylim(ymax=0.2)
-sff2d = sf2D4
-lpar=lpar4
-lperp=lperp4
-cs=ax3.contour(lpar,lperp,np.transpose(sff2d),levels=[sff2d[5,5],sff2d[5,10],sff2d[5,20],sff2d[5,40],sff2d[5,60],sff2d[5,80],sff2d[5,100]],linewidths=2)
-#plt.clabel(cs,inline=1,fontsize=10)
-ax3.set_title('(d) CB1a B field',fontsize=18)
-ax3.axes.xaxis.set_ticklabels([])
-ax3.axes.yaxis.set_ticklabels([])
-#ax3.set_ylabel(r'$l_{\perp}$',fontsize=16)
-#ax3.set_xlabel('$l_{\parallel}$',fontsize=16)
-#ax0.legend(loc='bottom left')
-#plt.axvline(x=25.2)
+# ax3 = plt.subplot(gs[3],aspect='equal')
+# ax3.set_xlim(xmax=0.2)
+# #ax0.set_ylim(ymin=10.0)
+# ax3.set_ylim(ymax=0.2)
+# sff2d = sf2D4
+# lpar=lpar4
+# lperp=lperp4
+# cs=ax3.contour(lpar,lperp,np.transpose(sff2d),levels=[sff2d[5,5],sff2d[5,10],sff2d[5,20],sff2d[5,40],sff2d[5,60],sff2d[5,80],sff2d[5,100]],linewidths=2)
+# #plt.clabel(cs,inline=1,fontsize=10)
+# ax3.set_title('(d) CB1a B field',fontsize=18)
+# ax3.axes.xaxis.set_ticklabels([])
+# ax3.axes.yaxis.set_ticklabels([])
+# #ax3.set_ylabel(r'$l_{\perp}$',fontsize=16)
+# #ax3.set_xlabel('$l_{\parallel}$',fontsize=16)
+# #ax0.legend(loc='bottom left')
+# #plt.axvline(x=25.2)
 
-ax4 = plt.subplot(gs[4],aspect='equal')
-ax4.set_xlim(xmax=0.2)
-#ax0.set_ylim(ymin=10.0)
-ax4.set_ylim(ymax=0.2)
-sff2d = sf2D5
-lpar=lpar5
-lperp=lperp5
-cs=ax4.contour(lpar,lperp,np.transpose(sff2d),levels=[sff2d[5,5],sff2d[5,10],sff2d[5,15],sff2d[5,30],sff2d[5,60],sff2d[5,100],sff2d[5,150]],linewidths=2)
-#plt.clabel(cs,inline=1,fontsize=10)
-ax4.set_title('(e) S1b v field',fontsize=18)
-ax4.set_ylabel(r'$l_{\perp}/L$',fontsize=18)
-ax4.set_xlabel('$l_{\parallel}/L$',fontsize=18)
-#ax0.legend(loc='bottom left')
-#plt.axvline(x=25.2)
+# ax4 = plt.subplot(gs[4],aspect='equal')
+# ax4.set_xlim(xmax=0.2)
+# #ax0.set_ylim(ymin=10.0)
+# ax4.set_ylim(ymax=0.2)
+# sff2d = sf2D5
+# lpar=lpar5
+# lperp=lperp5
+# cs=ax4.contour(lpar,lperp,np.transpose(sff2d),levels=[sff2d[5,5],sff2d[5,10],sff2d[5,15],sff2d[5,30],sff2d[5,60],sff2d[5,100],sff2d[5,150]],linewidths=2)
+# #plt.clabel(cs,inline=1,fontsize=10)
+# ax4.set_title('(e) S1b v field',fontsize=18)
+# ax4.set_ylabel(r'$l_{\perp}/L$',fontsize=18)
+# ax4.set_xlabel('$l_{\parallel}/L$',fontsize=18)
+# #ax0.legend(loc='bottom left')
+# #plt.axvline(x=25.2)
 
-ax5 = plt.subplot(gs[5],aspect='equal')
-ax5.set_xlim(xmax=0.2)
-#ax0.set_ylim(ymin=10.0)
-ax5.set_ylim(ymax=0.2)
-sff2d = sf2D6
-lpar=lpar6
-lperp=lperp6
-cs=ax5.contour(lpar,lperp,np.transpose(sff2d),levels=[sff2d[5,5],sff2d[5,10],sff2d[5,15],sff2d[5,30],sff2d[5,60],sff2d[5,100],sff2d[5,150]],linewidths=2)
-#plt.clabel(cs,inline=1,fontsize=10)
-ax5.set_title('(f) S4b v field',fontsize=18)
-ax5.axes.yaxis.set_ticklabels([])
-#ax5.set_ylabel(r'$l_{\perp}$',fontsize=16)
-ax5.set_xlabel('$l_{\parallel}/L$',fontsize=18)
-#ax0.legend(loc='bottom left')
-#plt.axvline(x=25.2)
+# ax5 = plt.subplot(gs[5],aspect='equal')
+# ax5.set_xlim(xmax=0.2)
+# #ax0.set_ylim(ymin=10.0)
+# ax5.set_ylim(ymax=0.2)
+# sff2d = sf2D6
+# lpar=lpar6
+# lperp=lperp6
+# cs=ax5.contour(lpar,lperp,np.transpose(sff2d),levels=[sff2d[5,5],sff2d[5,10],sff2d[5,15],sff2d[5,30],sff2d[5,60],sff2d[5,100],sff2d[5,150]],linewidths=2)
+# #plt.clabel(cs,inline=1,fontsize=10)
+# ax5.set_title('(f) S4b v field',fontsize=18)
+# ax5.axes.yaxis.set_ticklabels([])
+# #ax5.set_ylabel(r'$l_{\perp}$',fontsize=16)
+# ax5.set_xlabel('$l_{\parallel}/L$',fontsize=18)
+# #ax0.legend(loc='bottom left')
+# #plt.axvline(x=25.2)
 
-ax6 = plt.subplot(gs[6],aspect='equal')
-ax6.set_xlim(xmax=0.2)
-#ax0.set_ylim(ymin=10.0)
-ax6.set_ylim(ymax=0.2)
-sff2d = sf2D7
-lpar=lpar7
-lperp=lperp7
-cs=ax6.contour(lpar,lperp,np.transpose(sff2d),levels=[sff2d[5,5],sff2d[5,10],sff2d[5,20],sff2d[5,40],sff2d[5,60],sff2d[5,80],sff2d[5,100]],linewidths=2)
-#plt.clabel(cs,inline=1,fontsize=10)
-ax6.set_title('(g) C2a v field',fontsize=18)
-#ax6.set_ylabel(r'$l_{\perp}$',fontsize=16)
-ax6.axes.yaxis.set_ticklabels([])
-ax6.set_xlabel('$l_{\parallel}/L$',fontsize=18)
-#ax0.legend(loc='bottom left')
-#plt.axvline(x=25.2)
+# ax6 = plt.subplot(gs[6],aspect='equal')
+# ax6.set_xlim(xmax=0.2)
+# #ax0.set_ylim(ymin=10.0)
+# ax6.set_ylim(ymax=0.2)
+# sff2d = sf2D7
+# lpar=lpar7
+# lperp=lperp7
+# cs=ax6.contour(lpar,lperp,np.transpose(sff2d),levels=[sff2d[5,5],sff2d[5,10],sff2d[5,20],sff2d[5,40],sff2d[5,60],sff2d[5,80],sff2d[5,100]],linewidths=2)
+# #plt.clabel(cs,inline=1,fontsize=10)
+# ax6.set_title('(g) C2a v field',fontsize=18)
+# #ax6.set_ylabel(r'$l_{\perp}$',fontsize=16)
+# ax6.axes.yaxis.set_ticklabels([])
+# ax6.set_xlabel('$l_{\parallel}/L$',fontsize=18)
+# #ax0.legend(loc='bottom left')
+# #plt.axvline(x=25.2)
 
-ax7 = plt.subplot(gs[7],aspect='equal')
-ax7.set_xlim(xmax=0.2)
-#ax0.set_ylim(ymin=10.0)
-ax7.set_ylim(ymax=0.2)
-sff2d = sf2D8
-lpar=lpar8
-lperp=lperp8
-cs=ax7.contour(lpar,lperp,np.transpose(sff2d),levels=[sff2d[5,5],sff2d[5,10],sff2d[5,20],sff2d[5,40],sff2d[5,60],sff2d[5,80],sff2d[5,100]],linewidths=2)
-#plt.clabel(cs,inline=1,fontsize=10)
-ax7.set_title('(h) CB1a v field',fontsize=18)
-ax7.axes.yaxis.set_ticklabels([])
-#ax7.set_ylabel(r'$l_{\perp}$',fontsize=16)
-ax7.set_xlabel('$l_{\parallel}/L$',fontsize=18)
-#ax0.legend(loc='bottom left')
-#plt.axvline(x=25.2)
-
-
+# ax7 = plt.subplot(gs[7],aspect='equal')
+# ax7.set_xlim(xmax=0.2)
+# #ax0.set_ylim(ymin=10.0)
+# ax7.set_ylim(ymax=0.2)
+# sff2d = sf2D8
+# lpar=lpar8
+# lperp=lperp8
+# cs=ax7.contour(lpar,lperp,np.transpose(sff2d),levels=[sff2d[5,5],sff2d[5,10],sff2d[5,20],sff2d[5,40],sff2d[5,60],sff2d[5,80],sff2d[5,100]],linewidths=2)
+# #plt.clabel(cs,inline=1,fontsize=10)
+# ax7.set_title('(h) CB1a v field',fontsize=18)
+# ax7.axes.yaxis.set_ticklabels([])
+# #ax7.set_ylabel(r'$l_{\perp}$',fontsize=16)
+# ax7.set_xlabel('$l_{\parallel}/L$',fontsize=18)
+# #ax0.legend(loc='bottom left')
+# #plt.axvline(x=25.2)
 
 
-plt.savefig('test.eps', bbox_inches='tight',transparent=False)
+
+
+#plt.savefig('test.eps', bbox_inches='tight',transparent=False)
 plt.show()
 
